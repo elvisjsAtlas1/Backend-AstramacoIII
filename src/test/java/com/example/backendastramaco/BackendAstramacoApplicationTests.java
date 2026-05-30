775d7d12b7b87c6979d1e4e61c8d603012936201
elvisjsAtlas1 <chat02jselvis@gmail.com>
2026-05-30 13:55:53 -0500

    mejorando Test Vacio

--- src/test/java/com/example/backendastramaco/BackendAstramacoApplicationTests.java
+++ src/test/java/com/example/backendastramaco/BackendAstramacoApplicationTests.java
@@ -1,15 +1,23 @@
 package com.example.backendastramaco;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class BackendAstramacoApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        // Verifica que el contexto de Spring se carga correctamente
+        assertNotNull(applicationContext, "El contexto de la aplicación debería cargarse correctamente");
     }
-
 }
